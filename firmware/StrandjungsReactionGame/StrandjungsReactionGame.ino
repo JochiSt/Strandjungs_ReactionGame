7e59aa37c565dd7784faa6aaf8bc43e5aeaef1b8
JochiSt <jochen.steinmann@web.de>
2022-06-29 14:26:40 +0200

    added pins from schematic

--- firmware/StrandjungsReactionGame/StrandjungsReactionGame.ino
+++ firmware/StrandjungsReactionGame/StrandjungsReactionGame.ino
@@ -12,32 +12,41 @@
    If trigger button is pressed, wait 2 to 10 seconds, then release a wooden rod, which should be catched by the player.
 */
 
-
+// array of the magnet outputs
+int magnets = {8, 9, 10, 11, 12};
 
 void setup() {
   // put your setup code here, to run once:
 
   // outputs to 5 electro magnets
+  for(int i=0; i<5; i++){
+    pinMode( magnets[i], OUTPUT );
+    digitalWrite( magnets[i], HIGH);
+  }
 
   // trigger button input
+  pinMode( 5, INPUT_PULLUP );
+
+  // reset button
+  pinMode( 6, INPUT_PULLUP );
 
   // mode selection inputs
-
+  pinMode( 2, INPUT_PULLUP );
+  pinMode( 3, INPUT_PULLUP );
 }
 
 void loop() {
+  // check mode pins
+  bool sel_0 = digitalRead(2);
+  bool sel_1 = digitalRead(3);
+  
   // wait until button is pressed
 
   // if button is pressed dice rod and wait random time
-  rod = random(0, 4); // dice the rod, which should be dropped
+  float rod = random(0, 4); // dice the rod, which should be dropped
 
   timewait = random(0, 800); // wait between 0 and 800 (unit 10ms)
   delay(timewait * 10 + 2000); // unit ms
-
-
-
-
-  // check, which mode we are operating
 
 
 }
